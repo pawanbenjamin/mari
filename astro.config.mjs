2afe56290e16deca3971e0e382693271e060a649
pawanbenjamin <pawanbenjamin@gmail.com>
2023-02-24 13:42:10 -0600

    css tweaks in header

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,6 @@
-import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-import react from "@astrojs/react";
-
-// https://astro.build/config
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
-});
+  integrations: []
+})
