b65bc510a6f4b289b236304e283789f26eb1867e
pawanbenjamin <pawanbenjamin@gmail.com>
2023-03-25 14:39:04 -0500

    netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
 import image from "@astrojs/image";
+
+// https://astro.build/config
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  integrations: [image()]
+  integrations: [image()],
+  adapter: netlify()
 });
