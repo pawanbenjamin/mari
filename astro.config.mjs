cb4ededd97feea14790bce9a56255cfba0bc5773
pawanbenjamin <pawanbenjamin@gmail.com>
2023-04-18 23:19:56 -0500

    sitemap plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
 import netlify from "@astrojs/netlify/functions";
+
+// https://astro.build/config
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: netlify()
+  adapter: netlify(),
+  integrations: [sitemap()]
 });
